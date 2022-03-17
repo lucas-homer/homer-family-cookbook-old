5e5389ecc0e1dadc7e5921b4b5c2fd90eb36882c
lucas homer <lucas.homer@gmail.com>
2022-03-17 16:41:54 -0700

    implement favorite and unfavorite recipe

--- app/models/recipe.server.ts
+++ app/models/recipe.server.ts
@@ -1,9 +1,19 @@
-import { Ingredient, MealType, Note, Recipe, User } from "@prisma/client";
+import {
+  Ingredient,
+  MealType,
+  Note,
+  Recipe,
+  User,
+  UsersFavoriteRecipes,
+} from "@prisma/client";
 import { db } from "~/models/db.server";
 
 export type GetRecipeResponse = Recipe & { mealTypes: MealType[] } & {
   ingredients: Ingredient[];
-} & { notes: Note[] } & { user: User };
+} & { notes: Note[] } & { user: User } & {
+  favoritedUsers: UsersFavoriteRecipes[];
+};
+
 export async function getRecipe(recipeId: Recipe["id"]) {
   return db.recipe.findUnique({
     where: { id: recipeId },
@@ -12,6 +22,38 @@ export async function getRecipe(recipeId: Recipe["id"]) {
       ingredients: true,
       notes: true,
       author: true,
+      favoritedUsers: true,
+    },
+  });
+}
+
+export async function favoriteRecipe({
+  recipeId,
+  userId,
+}: {
+  recipeId: Recipe["id"];
+  userId: User["id"];
+}) {
+  return db.usersFavoriteRecipes.create({
+    data: {
+      recipeId,
+      userId,
+    },
+  });
+}
+export async function unfavoriteRecipe({
+  recipeId,
+  userId,
+}: {
+  recipeId: Recipe["id"];
+  userId: User["id"];
+}) {
+  return db.usersFavoriteRecipes.delete({
+    where: {
+      recipeId_userId: {
+        userId,
+        recipeId,
+      },
     },
   });
 }
--- app/routes/recipe/$recipeId.tsx
+++ app/routes/recipe/$recipeId.tsx
@@ -1,8 +1,21 @@
-import { LoaderFunction, useLoaderData } from "remix";
-import { getRecipe, GetRecipeResponse } from "~/models/recipe.server";
+import { User } from "@prisma/client";
+import { ActionFunction, Form, LoaderFunction, useLoaderData } from "remix";
+import {
+  favoriteRecipe,
+  unfavoriteRecipe,
+  getRecipe,
+  GetRecipeResponse,
+} from "~/models/recipe.server";
+import { getUserId } from "~/models/user.server";
+import { requireUserId } from "~/utils/auth.server";
 
-export const loader: LoaderFunction = async ({ params }) => {
+type LoaderData = {
+  recipeData: GetRecipeResponse;
+  userId: User["id"] | null;
+};
+export const loader: LoaderFunction = async ({ params, request }) => {
   const recipeData = await getRecipe(Number(params.recipeId));
+  const userId = await getUserId(request);
 
   if (!recipeData) {
     throw new Response("What a joke! Not found.", {
@@ -10,15 +23,66 @@ export const loader: LoaderFunction = async ({ params }) => {
     });
   }
 
-  return recipeData;
+  return {
+    userId,
+    recipeData,
+  };
+};
+
+const actionIds = {
+  favorite: "favorite",
+  unfavorite: "unfavorite",
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const userId = await requireUserId(request);
+  const recipeId = Number(params.recipeId);
+
+  const form = await request.formData();
+  const actionId = form.get("actionId");
+
+  if (actionId === actionIds.favorite) {
+    const response = await favoriteRecipe({
+      userId,
+      recipeId,
+    });
+    console.log("response", response);
+  }
+
+  if (actionId === actionIds.unfavorite) {
+    await unfavoriteRecipe({
+      userId,
+      recipeId,
+    });
+  }
+  return null;
 };
 
 export default function Recipe() {
-  const recipeData = useLoaderData<GetRecipeResponse>();
+  const { recipeData, userId } = useLoaderData<LoaderData>();
+
+  const isRecipeFavorited = recipeData.favoritedUsers.some(
+    (item) => item.userId === userId
+  );
+  const favoriteButtonText = isRecipeFavorited
+    ? "REMOVE FROM FAVORITES"
+    : "FAVORITE THIS RECIPE";
 
   return (
     <div>
-      <h1 className="text-4xl mb-8">{recipeData.title}</h1>
+      <div className="mb-8">
+        <h1 className="text-4xl mb-8">{recipeData.title}</h1>
+        <Form method="post">
+          <input
+            type="hidden"
+            name="actionId"
+            value={
+              isRecipeFavorited ? actionIds.unfavorite : actionIds.favorite
+            }
+          />
+          <button type="submit">{favoriteButtonText}</button>
+        </Form>
+      </div>
       <ul className="pl-8">
         {recipeData.ingredients?.map((ingredient) => (
           <li key={ingredient.id} className="text-xl mb-4">
